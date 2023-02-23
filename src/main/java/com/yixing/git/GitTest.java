02f998b35e2392f3be8698efc60ea919dcc6ec0b
ASUS <3438963405@qq.com>
2023-02-23 18:48:19 +0800

    hot-fix commit2

--- src/main/java/com/yixing/git/GitTest.java
+++ src/main/java/com/yixing/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello test git2222!!!");
         System.out.println("hello test git33333!!!");
         System.out.println("hello test git444444!!!");
+        System.out.println("hello test git hot-fix commit!!!");
 
     }
 }
