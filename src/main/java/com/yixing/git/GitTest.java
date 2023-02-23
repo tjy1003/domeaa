e40f7695868cb669cc537ba4fcb07e0a1fc1c605
ASUS <3438963405@qq.com>
2023-02-23 16:37:59 +0800

    first commit

--- src/main/java/com/yixing/git/GitTest.java
+++ src/main/java/com/yixing/git/GitTest.java
@@ -3,5 +3,7 @@ package com.yixing.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello test git!!!");
+        System.out.println("hello test git2222!!!");
+
     }
 }
